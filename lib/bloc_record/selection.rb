f3c63be55cd3605bb09cf6fb34b9e2bd26978b5b
Conrad Pacesa <conradpacesa@gmail.com>
2018-01-08 21:55:05 -0500

    Completed checkpoint 2 assignment

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -9,5 +9,15 @@ module Selection
 
     data = Hash[columns.zip(row)]
     new(data)
-  end 
+  end
+
+  def find_by(attribute, value)
+    row = connection.get_first_row <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      WHERE #{attribute} = #{value};
+    SQL
+
+    data = Hash[columns.zip(row)]
+    new(data)
+  end
 end
