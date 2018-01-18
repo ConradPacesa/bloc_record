36f968f5d1c17643650ef0bf038ad207e3400381
Conrad Pacesa <conradpacesa@gmail.com>
2018-01-18 16:02:23 -0500

    Completed select assignment

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -2,6 +2,10 @@ require 'sqlite3'
 
 module Selection
   def find(*ids)
+    ids.each do |id|
+      validate_int(id)
+    end
+
     if ids.length == 1 
       find_one(ids.first)
     else 
@@ -15,6 +19,8 @@ module Selection
   end
 
   def find_one(id)
+    validate_int(id)
+
     row = connection.get_first_row <<-SQL
       SELECT #{columns.join ","} FROM #{table}
       WHERE id = #{id};
@@ -24,6 +30,10 @@ module Selection
   end
 
   def find_by(attribute, value)
+    if !attribute.is_a?(String) || !value.is_a?(String)
+      raise ArgumentError, "Attribute and value must be strings."
+    end
+
     row = connection.get_first_row <<-SQL
       SELECT #{columns.join ","} FROM #{table}
       WHERE #{attribute} = #{BlocRecord::Utility.sql_strings(value)};
@@ -33,6 +43,8 @@ module Selection
   end
 
   def take(num=1)
+    validate_int(num)
+
     if num > 1
       rows = connection.execute <<-SQL
         SELECT #{columns.join ","} FROM #{table}
@@ -82,7 +94,61 @@ module Selection
     rows_to_array(rows)
   end
 
-  private 
+  def method_missing(m, *args, &block)
+    attribute = m.to_s
+    attribute.slice! "find_by_"
+    value = args[0]
+
+    row = connection.get_first_row <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      WHERE #{attribute} = #{BlocRecord::Utility.sql_strings(value)};
+    SQL
+
+    init_object_from_row(row)
+  end
+
+  def find_each(start: 0, batch_size: 1000)
+    offset = start
+
+    loop do 
+      rows = connection.execute <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        WHERE id > #{offset}
+        ORDER BY id ASC LIMIT #{batch_size};
+      SQL
+
+      result = rows_to_array(rows)
+      result.each do |r|
+        yield(r)
+        if r.id == self.last.id
+          return
+        end
+      end
+      offset += batch_size
+    end
+  end
+
+  def find_in_batches(start: 0, batch_size: 1000, &block)
+    offset = start
+
+    loop do
+      rows = connection.execute <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        WHERE id > #{offset}
+        ORDER BY id ASC LIMIT #{batch_size};
+      SQL
+
+      result = rows_to_array(rows)
+      block.call result
+
+      if result.last.id == self.last.id
+        return
+      end
+      offset += batch_size
+    end
+  end
+
+  private
   def init_object_from_row(row)
     if row
       data = Hash[columns.zip(row)]
@@ -92,5 +158,14 @@ module Selection
 
   def rows_to_array(rows)
     rows.map { |row| new(Hash[columns.zip(row)])}
+  end
+
+  def validate_int(int)
+    if !int.is_a?(Integer)
+      raise ArgumentError, "#{int} must be of type integer."
+    end
+    if int < 1
+      raise ArgumentError, "#{int} must be greater than 0."
+    end
   end
 end
