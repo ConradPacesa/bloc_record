351671fd899cc8746434dc30ee1993331e3f416f
Conrad Pacesa <conradpacesa@gmail.com>
2018-01-22 21:06:15 -0500

    Completed destroy section

--- lib/bloc_record/persistence.rb
+++ lib/bloc_record/persistence.rb
@@ -35,6 +35,10 @@ module Persistence
     self.class.update(self.id, updates)
   end
 
+  def destroy
+    self.class.destroy(self.id)
+  end
+
   def method_missing(m, *args)
     attribute = m.to_s
     attribute.slice! "update_"
@@ -86,6 +90,37 @@ module Persistence
         UPDATE #{table}
         SET #{updates_array * ","} #{where_clause}
       SQL
+      true
+    end
+
+    def destroy(*id)
+      if id.length > 1
+        where_clause = "WHERE id IN (#{id.join(",")});"
+      else 
+        where_clause = "WHERE id = #{id.first};"
+      end
+      connection.execute <<-SQL 
+        DELETE FROM #{table} #{where_clause}
+      SQL
+
+      true
+    end
+
+    def destroy_all(conditions_hash=nil)
+      if conditions_hash && !conditions_hash.empty?
+        conditions_hash = BlocRecord::Utility.convert_keys(conditions_hash)
+        conditions = conditions_hash.map { |key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}.join(" and ")
+
+        connection.execute <<-SQL
+          DELETE FROM #{table}
+          WHERE #{conditions};
+        SQL
+      else
+        connection.execute <<-SQL
+          DELETE FROM #{table};
+        SQL
+      end
+      
       true
     end
   end
